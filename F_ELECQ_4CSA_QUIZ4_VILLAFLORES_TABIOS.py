461f0d6161cca6f4f85967107de43985c196754f
LCVillaflores <52596850+LCVillaflores@users.noreply.github.com>
2019-11-09 11:45:07 +0800

    Added Audio Files and Output. Fixed Grammar

--- F_ELECQ_4CSA_QUIZ4_VILLAFLORES_TABIOS.py
+++ F_ELECQ_4CSA_QUIZ4_VILLAFLORES_TABIOS.py
@@ -2,33 +2,18 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-audio = 'harvard.wav'
+audio = '5_person.wav'
 
 with sr.AudioFile(audio) as source:
-    audio = r.record(source)
-    print('Done!')
+	audio = r.record(source)
+	print('Processing')
 
 try:
-    value = r.recognize_google(audio)
-
-    if str is bytes:
-        result = u"{}".format(value).encode("utf-8")
-
-    else:
-        result = "{}".format(value)
-
-    with open("Output.txt", "a") as f:
-        f.write(result)
-    print(result)
-
-except sr.UnknownValueError:
-    print("")
-
-except sr.RequestError as e:
-    print("{0}".format(e))
-
-except KeyboardInterrupt:
-    pass
+	value = r.recognize_google(audio)
+	file = open("5P.txt", "a")
+	file.write(value)
+	print('Finished Processing')
+	print(value)
 
 except Exception as e:
-    print(e)
+	print(e)
